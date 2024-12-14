393a7db9bc487d13caf419c26e1b1f08d2fa02f0
Om Jadav <omjadav04@gmail.com>
2024-12-14 19:40:38 +0530

    Set-up completed

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  variable: "--font-ibm-plex-serif",
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "RadiantBank",
+  description: "Banking that Shines Bright",
+  icons: "/icons/logo.svg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
